b93b983823a88668d1a51f6145a8381deda71e75
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-07-11 21:42:41 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sunny Balloons`,
+    description: `Decorate your events with fun and attractive balloon decorations!`,
+    author: `Sharon Kim`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
