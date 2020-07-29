0a7c01cc9b695c888ae06db175e7373643ab7607
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-07-29 17:25:24 -0400

    added styling to header component

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import Jumbotron from "react-bootstrap/Jumbotron"
+import headerStyles from "./header.module.css"
 
 // const Header = ({ siteTitle }) => (
 //   <header
@@ -37,15 +38,14 @@ const Header = ({ siteTitle }) => {
   return (  
     <div>
       <Jumbotron>
-        <div>
+        <div className={headerStyles.header}>
           <h1>Sunny Balloons</h1>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/events">Events</Link>
-            <Link to="/contact">Contact</Link>
+          <nav className={headerStyles.navbar}>
+            <Link className={headerStyles.navItem} to="/">Home</Link>
+            <Link className={headerStyles.navItem} to="/events">Events</Link>
+            <Link className={headerStyles.navItem} to="/contact">Contact</Link>
           </nav>
         </div>
-        
       </Jumbotron>
     </div>  
   )
