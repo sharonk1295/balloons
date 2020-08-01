6083331ce3b8bfc056c08641347229ae008df43c
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-07-31 23:49:42 -0400

    added basic content to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,6 +8,9 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Contact" />
     <h1>Hi from the contact page</h1>
+    {/* insert photo */}
+    <h3>Sunny Kim</h3>
+    <p>Enthusiastic about bringing life and joy to events, Sunny takes pride in her work and producing fun and cute decor for various occasions. She is especially experienced in decorating for kids' birthday parties and graduation celebrations. She is very easy to work with and get along with. She has a passion for working with balloons even on her free time, so you can depend on her to produce very pleasing balloon decorations!</p>
   </Layout>
 )
 
