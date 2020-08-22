9a8065814007e1a9f41d730abec2831765c4408a
Sharon Kim <carolinek@SKs-MacBook-Air.lan>
2020-08-22 15:47:27 -0400

    styled index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,8 @@ const IndexPage = () => {
   query {
     backPic: file(relativePath: { eq: "balloons_bg3.jpg" }) {
       childImageSharp {
-        fluid(quality: 100) {
+        fluid(maxWidth: 3200, maxHeight: 3200, quality: 100) {
           ...GatsbyImageSharpFluid
-          ...GatsbyImageSharpFluidLimitPresentationSize
         }
       }
     }
@@ -27,10 +26,12 @@ const IndexPage = () => {
     <BackgroundImage className={indexStyles.bgPic} fluid={data.backPic.childImageSharp.fluid} fadeIn={true}>
       <Layout>
         <SEO title="Home" />
-          <h1>Let me help you bring your imagination to life!</h1>
-          <p>Through balloon decorations, your kids' parties will be more vibrant.</p>
-          <h3>View some decoration examples here</h3>
-          <Link to='/events'><button className={indexStyles.button}>Explore More</button></Link>
+          <div className={indexStyles.mainText}>
+            <h1>Let me help you bring your imagination to life!</h1>
+            <p>Through balloon decorations, your kids' parties will be more vibrant.</p>
+            <h3>View some decoration examples here</h3>
+            <Link to='/events'><button className={indexStyles.button}>Explore More</button></Link>
+          </div>
       </Layout>
     </BackgroundImage>
   )
